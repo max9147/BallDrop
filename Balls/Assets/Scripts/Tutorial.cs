cfb97612c091aebcc0ee9a6db1dfbc907739f9a0
max9147 <m.chizh99@gmail.com>
2021-10-28 17:51:47 +0300

    Fix

--- Balls/Assets/Scripts/Tutorial.cs
+++ Balls/Assets/Scripts/Tutorial.cs
@@ -12,14 +12,19 @@ public class Tutorial : MonoBehaviour
 
     private bool isPlaying = false;
     private int curStep = 0;
+    private float waitTime = 0;
     private Touch touch;
 
     private void Update()
     {
+        if (isPlaying)
+        {
+            waitTime += Time.deltaTime;
+        }
         if (Input.touchCount > 0)
         {
             touch = Input.GetTouch(0);
-            if (touch.phase == TouchPhase.Began && isPlaying)
+            if (touch.phase == TouchPhase.Began && isPlaying && waitTime > 1f && !tutorialMenu.activeInHierarchy)
             {
                 NextStep();
             }
@@ -34,12 +39,14 @@ public class Tutorial : MonoBehaviour
 
     public void StartTutorial()
     {
+        waitTime = 0;
         tutorial.SetActive(true);
         tutorialMenu.SetActive(false);
     }
 
     public void NextStep()
     {
+        waitTime = 0;
         curStep++;
         if (curStep == 3)
         {
